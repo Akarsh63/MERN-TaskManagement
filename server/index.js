948f1bbef1616ef7a6e718660795fbf845c760d0
Akarsh63 <akarsh.1@iitj.ac.in>
2023-06-14 18:45:55 +0530

    Frontend added to Backend

--- server/index.js
+++ server/index.js
@@ -5,12 +5,15 @@ const app = express();
 const tasksRouter = require('./routes/tasks.js');
 require("dotenv").config();
 app.get('/', (req, res) => res.send('/tasks ---> for tasks data'));
+const corsOptions = {
+  origin: "https://mern-taskmanagementappclient.onrender.com" 
+}
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
+
 app.use('/tasks', tasksRouter);
 
-mongoose.connect(
-    "mongodb+srv://akarsh:akarsh@taskmanagementapplicati.qoniiwm.mongodb.net/taskmanagementdatabase?retryWrites=true&w=majority",
+mongoose.connect(process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -18,6 +21,7 @@ mongoose.connect(
 ).then(async () => {
   console.log('Connected to MongoDB');});
 
-const port = 8083;
-
-app.listen(process.env.PORT || port, () => console.log(`Server running on port ${port}`));
+const PORT = process.env.PORT || 8083
+app.listen(PORT, () => {
+      console.log(`App is Listening on PORT ${PORT}`);
+})
